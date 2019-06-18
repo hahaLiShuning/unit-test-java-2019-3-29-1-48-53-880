0c60479b5770db65d145375e9cfacd13f85f13e7
LIShuning <lishiuning1955142623@outlook.com>
2019-06-18 12:44:34 +0800

    test for AnswerGenerator completed

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,48 @@
 package tw.core.generator;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
+    private RandomIntGenerator mockRandomIntGenerator;
+
+    @Before
+    public void setUp() {
+        mockRandomIntGenerator = mock(RandomIntGenerator.class);
+    }
+
+    @Test(expected = OutOfRangeAnswerException.class)
+    public void should_throws_out_of_range_exception_when_input_number_id_not_vary_from_0_to_9() throws OutOfRangeAnswerException {
+        //given
+        when(mockRandomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 10");
+
+        //when
+        AnswerGenerator answerGenerator = new AnswerGenerator(mockRandomIntGenerator);
+        answerGenerator.generate();
+    }
+
+    @Test
+    public void should_generate_answer() throws OutOfRangeAnswerException {
+        //given
+        when(mockRandomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 4");
+
+        //when
+        AnswerGenerator answerGenerator = new AnswerGenerator(mockRandomIntGenerator);
+        Answer answer = answerGenerator.generate();
+
+        //then
+        Assert.assertNotNull(answer);
+    }
 }
 
